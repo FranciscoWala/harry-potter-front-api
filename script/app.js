58d03704c0adc5707f1c264dc680e3922099d85b
FRANCISCO WALA NORONHA MACEDO <25203678@CFP127ALUNO.LOCAL>
2026-05-12 15:46:22 -0300

    feat: persoagens com dados corretos

---
+++ script/app.js
@@ -0,0 +1,42 @@
+async function criarCard(dados) {
+    const card = document.createElement('div')
+    card.className = 'card'
+
+    const foto = document.createElement('img')
+
+    foto.src = dados.attributes.image || 'https://placehold.co/200x200?text=Sem+Foto'
+    foto.alt = dados.attributes.name
+
+    const casa = document.createElement('span')
+    casa.textContent = dados.attributes.house
+    console.log(casa)
+
+    const name = document.createElement('span')
+    name.textContent = dados.attributes.name
+
+    const sangueTipo = document.createElement('span')
+    sangueTipo.textContent = dados.attributes.blood_status
+
+    card.append(foto, name, sangueTipo, casa)
+
+    return card
+}
+
+const getDados = async function(){
+    let cardsContainer = document.getElementById('containerCharacters')
+
+    let url = `https://api.potterdb.com/v1/characters`
+    let response = await fetch(url)
+    let dadosJson = await response.json()
+
+    const listaPersonagens = dadosJson.data 
+
+    const cardsPrometidos = listaPersonagens.map(personagemJson => criarCard(personagemJson))
+    
+
+    const todosOsCards = await Promise.all(cardsPrometidos)
+
+    cardsContainer.replaceChildren(...todosOsCards)
+}
+
+getDados()
